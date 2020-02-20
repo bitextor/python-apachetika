795be347f5f381751674c53d0cc04c2926dddb1d
Leopoldo Pla <lpla@dlsi.ua.es>
2020-02-20 16:20:36 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 import tarfile
 from fnmatch import fnmatch
 from os.path import basename, exists, dirname, abspath, join
-#from distutils.core import setup
-from setuptools import setup
+from distutils.core import setup
+#from setuptools import setup
 
 try:
     from urllib import urlretrieve
